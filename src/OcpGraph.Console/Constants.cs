141b33492ca82629f0b64e565a51a31ed7ffa9c9
Stevo John <stevehuwjohn@gmail.com>
2026-06-08 21:06:06 +0100

    WIP

---
+++ src/OcpGraph.Console/Constants.cs
@@ -0,0 +1,6 @@
+namespace OcpGraph.Console;
+
+public static class Constants
+{
+    public const int CoordinateScalingFactor = 10_000_000;
+}
--- src/OcpGraph.Core/Models/Node.cs
+++ src/OcpGraph.Core/Models/Node.cs
@@ -1,3 +1,19 @@
+using System.Reflection.Metadata;
+
 namespace OcpGraph.Core.Models;
 
-public record Node(long Id) : Object(Id);
+public record Node(long Id) : Object(Id)
+{
+    private const int CoordinateScalingFactor = 10_000_000;
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public Node(BinaryReader reader) : this(reader.Read7BitEncodedInt64())
+    {
+        Latitude = reader.Read7BitEncodedInt() * CoordinateScalingFactor;
+        
+        Longitude = reader.Read7BitEncodedInt() * CoordinateScalingFactor;
+    }
+}
