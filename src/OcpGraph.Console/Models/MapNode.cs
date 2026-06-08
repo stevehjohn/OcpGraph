14148612ab76cd32c2b73f3e7509f4c96821fed4
Stevo John <stevehuwjohn@gmail.com>
2026-06-08 20:52:08 +0100

    WIP

--- src/OcpGraph.Console/Models/MapNode.cs
+++ src/OcpGraph.Console/Models/MapNode.cs
@@ -2,5 +2,14 @@ namespace OcpGraph.Console.Models;
 
 public sealed record MapNode(long Id) : MapObject(Id)
 {
-    public MapNode(OsmSharp.Node node) : this(GetId(node.Id)) { }
+    public double Latitude { get; }
+    
+    public double Longitude { get; }
+
+    public MapNode(OsmSharp.Node node) : this(GetId(node.Id))
+    {
+        Latitude = node.Latitude ?? throw new ArgumentException("Latitude must have a value.", nameof(node.Latitude));
+
+        Longitude = node.Longitude ?? throw new ArgumentException("Longitude must have a value.", nameof(node.Longitude));
+    }
 }
