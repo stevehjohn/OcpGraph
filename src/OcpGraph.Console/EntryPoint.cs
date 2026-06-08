b6c5c2e0f30ed4eb947e16e2202950c8d5554cc5
Stevo John <stevehuwjohn@gmail.com>
2026-06-08 23:47:51 +0100

    WIP

--- src/OcpGraph.Console/EntryPoint.cs
+++ src/OcpGraph.Console/EntryPoint.cs
@@ -39,7 +39,7 @@ public static class EntryPoint
 
         Write($"\n\nGraph loaded in {stopwatch.Elapsed.Seconds}s\n\n");
 
-        var way = graph.FindNearestWay(51.4725143, -2.6329321);
+        var way = graph.FindNearestWay(51.5036356, -3.5646179);
 
         WriteLine($"Nearest way: {graph.GetName(way)}");
     }
