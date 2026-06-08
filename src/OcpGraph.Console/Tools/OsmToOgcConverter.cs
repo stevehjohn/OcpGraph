ef93847e18f0b54874f7ee6537312e60a677560f
Stevo John <stevehuwjohn@gmail.com>
2026-06-08 21:22:27 +0100

    Revert

--- src/OcpGraph.Console/Tools/OsmToOgcConverter.cs
+++ src/OcpGraph.Console/Tools/OsmToOgcConverter.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using OcpGraph.Console.DataProviders;
 using OcpGraph.Console.Models;
+using OcpGraph.Core.Models;
 using static System.Console;
 
 namespace OcpGraph.Console.Tools;
@@ -10,26 +11,26 @@ namespace OcpGraph.Console.Tools;
 public static class OsmToOgcConverter
 {
     private const int CoordinateScalingFactor = 10_000_000;
-    
+
     public static void ConvertData()
     {
         IMapDataProvider provider = new OsmMapDataProvider();
-        
+
         var count = 0;
-        
+
         var stopwatch = Stopwatch.StartNew();
-        
+
         var lastUpdateMilliseconds = stopwatch.ElapsedMilliseconds;
 
         using var wayWriter = new BinaryWriter(File.Create("./data/ways.ogc"));
 
         using var nameWriter = new BinaryWriter(File.Create("./data/names.ogc"));
 
-        using var nodeWriter = new BinaryWriter(File.Create("./data/nodes.ogc"));
-
         var names = new Dictionary<string, int>();
 
         var id = 1;
+
+        var nodeIds = new HashSet<long>();
 
         foreach (var mapObject in provider.Read())
         {
@@ -38,14 +39,14 @@ public static class OsmToOgcConverter
             if (stopwatch.ElapsedMilliseconds - lastUpdateMilliseconds > 500)
             {
                 lastUpdateMilliseconds = stopwatch.ElapsedMilliseconds;
-                
+
                 WriteLine($"{count:N0} nodes in {stopwatch.Elapsed.TotalSeconds:N2}s, ({provider.Progress:N2}%).");
             }
 
             if (mapObject is MapWay way)
             {
                 wayWriter.Write7BitEncodedInt64(way.Id);
-                
+
                 wayWriter.Write((byte) way.Type);
 
                 if (! string.IsNullOrEmpty(way.Name))
@@ -53,7 +54,7 @@ public static class OsmToOgcConverter
                     if (names.TryAdd(way.Name, id))
                     {
                         nameWriter.Write7BitEncodedInt(id++);
-            
+
                         nameWriter.Write(way.Name);
                     }
 
@@ -69,17 +70,17 @@ public static class OsmToOgcConverter
                     if (names.TryAdd(way.Designation, id))
                     {
                         nameWriter.Write7BitEncodedInt(id++);
-            
+
                         nameWriter.Write(way.Designation);
                     }
-                
+
                     wayWriter.Write7BitEncodedInt(names[way.Designation]);
                 }
                 else
                 {
                     wayWriter.Write7BitEncodedInt(0);
                 }
-                
+
                 wayWriter.Write(way.MaxSpeed ?? 0);
 
                 wayWriter.Write((byte) way.Direction);
@@ -89,23 +90,40 @@ public static class OsmToOgcConverter
                 foreach (var item in way.Nodes)
                 {
                     wayWriter.Write7BitEncodedInt64(item);
+
+                    if (way.Type != WayType.NoVehicles)
+                    {
+                        nodeIds.Add(item);
+                    }
                 }
-                
-                continue;
             }
-            
-            if (mapObject is MapNode node)
+        }
+
+        using var nodeWriter = new BinaryWriter(File.Create("./data/nodes.ogc"));
+
+        foreach (var mapObject in provider.Read())
+        {
+            count++;
+
+            if (stopwatch.ElapsedMilliseconds - lastUpdateMilliseconds > 500)
+            {
+                lastUpdateMilliseconds = stopwatch.ElapsedMilliseconds;
+
+                WriteLine($"{count:N0} nodes in {stopwatch.Elapsed.TotalSeconds:N2}s, ({provider.Progress:N2}%).");
+            }
+
+            if (mapObject is MapNode node && nodeIds.Contains(node.Id))
             {
                 nodeWriter.Write7BitEncodedInt64(node.Id);
-                
+
                 nodeWriter.Write7BitEncodedInt((int) (node.Latitude * CoordinateScalingFactor));
-                
+
                 nodeWriter.Write7BitEncodedInt((int) (node.Longitude * CoordinateScalingFactor));
             }
         }
 
         stopwatch.Stop();
-        
+
         WriteLine($"{count:N0} map objects indexed in {stopwatch.Elapsed.TotalSeconds}s.");
     }
 }
