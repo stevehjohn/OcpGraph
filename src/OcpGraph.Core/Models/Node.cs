b406516cf303896743a8f19287f67234bd503ae9
Stevo John <stevehuwjohn@gmail.com>
2026-06-08 23:45:50 +0100

    WIP

--- src/OcpGraph.Core/Models/Node.cs
+++ src/OcpGraph.Core/Models/Node.cs
@@ -4,9 +4,9 @@ public record Node(long Id) : Object(Id)
 {
     private const double CoordinateScalingFactor = 10_000_000;
 
-    public double Latitude { get; set; }
+    public double Latitude { get; }
 
-    public double Longitude { get; set; }
+    public double Longitude { get; }
 
     public Node(BinaryReader reader) : this(reader.Read7BitEncodedInt64())
     {
