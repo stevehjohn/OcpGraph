88d118336f6d09363897fc5c12a4538f89f64284
Stevo John <stevehuwjohn@gmail.com>
2026-06-09 15:29:18 +0100

    WIP

---
+++ src/OcpGraph.Viewer/Infrastructure/TextManager.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OcpGraph.Viewer.Infrastructure;
+
+public class TextManager
+{
+    private readonly SpriteBatch _spriteBatch;
+
+    private readonly SpriteFont _font;
+
+    public TextManager(SpriteBatch spriteBatch, SpriteFont font)
+    {
+        _spriteBatch = spriteBatch;
+
+        _font = font;
+    }
+
+    public void DrawMessage(string message, int left, int top, Color? color = null, bool center = false)
+    {
+        if (message == null)
+        {
+            return;
+        }
+
+        color ??= Color.White;
+
+        if (center)
+        {
+            left -= (int) _font.MeasureString(message).X / 2;
+        }
+
+        for (var y = -2; y < 3; y++)
+        {
+            for (var x = -2; x < 3; x++)
+            {
+                _spriteBatch.DrawString(_font, message, new Vector2(left + x, top + y), Color.Black, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, .6f);
+            }
+        }
+
+        _spriteBatch.DrawString(_font, message, new Vector2(left, top), color.Value, 0, Vector2.Zero, Vector2.One, SpriteEffects.None, .7f);
+    }
+}
