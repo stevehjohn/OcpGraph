311c787f29855740e3072702fa2d56eac5626bf3
Stevo John <stevehuwjohn@gmail.com>
2026-06-09 15:19:18 +0100

    WIP

--- src/OcpGraph.Viewer/Display/Renderer.cs
+++ src/OcpGraph.Viewer/Display/Renderer.cs
@@ -4,5 +4,27 @@ namespace OcpGraph.Viewer.Display;
 
 public sealed class Renderer : Game
 {
+    private const int WindowWidth = 800;
+
+    private const int WindowHeight = 600;
+
+    private readonly GraphicsDeviceManager _graphics;
     
+    public Renderer()
+    {
+        _graphics = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = WindowWidth,
+            PreferredBackBufferHeight = WindowHeight
+        };
+
+        IsMouseVisible = true;
+    }
+    
+    protected override void Initialize()
+    {
+        Window.Title = "OcpGraph Viewer";
+        
+        base.Initialize();
+    }
 }
